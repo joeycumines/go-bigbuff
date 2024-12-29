e180248743885dac96d59652624444bcea79b2d5
Joseph Cumines <joeycumines@gmail.com>
2024-12-29 12:15:59 +1000

    refactor(CombineContext): use context.AfterFunc
    Aside from the general jankyness of the old implementation, this new one avoids
    _any_ risk of goroutine leak, unless used with custom context that doesn't
    implement the AfterFunc method. May still leak resources. Compatible behavior.

--- context.go
+++ context.go
@@ -110,100 +110,61 @@ func ChainAfterFunc(ctx context.Context, other context.Context, f func()) {
 	})
 }
 
-// CombineContext returns a context based on the ctx (first param), that will cancel when ANY of the other provided
-// context values cancel CAUTION this spawns one or more blocking goroutines, if you call this with contexts
-// that don't cancel in the reasonable lifetime of your application you will have a leak
+// CombineContext returns a context based on the ctx (first param), that will
+// cancel when ANY of the other provided context values cancel.
+//
+// WARNING: This implementation relies on at least one of the contexts being
+// cancelled, to facilitate cleanup. While the newer [context.AfterFunc] API
+// is leveraged to reduce the risk of resource leaks, it still deserves
+// significant care and attention.
 func CombineContext(ctx context.Context, others ...context.Context) context.Context {
 	if ctx == nil {
 		ctx = context.Background()
-	}
-	if ctx.Err() != nil {
+	} else if ctx.Err() != nil {
 		return ctx
 	}
-	done := make([]<-chan struct{}, 0, len(others))
+
+	// count number of non-nil contexts, and guard any cancels
+	n := 0
 	for _, other := range others {
-		if other == nil {
-			continue
+		if other != nil {
+			if other.Err() != nil {
+				ctx, cancel := context.WithCancel(ctx)
+				cancel()
+				return ctx
+			}
+			n++
 		}
-		if other.Err() != nil {
-			combined, cancel := context.WithCancel(ctx)
-			cancel()
-			return combined
-		}
-		done = append(done, other.Done())
 	}
-	if len(done) == 0 {
+	if n == 0 {
 		return ctx
 	}
-	combined, cancel := context.WithCancel(ctx)
-	go combineContextWorker(combined.Done(), cancel, done...)
-	return combined
+
+	// we will need to deregister all after funcs on cancel
+	stops := make(stopCallbackSlice, 0, n)
+
+	// this will be our result
+	ctx, cancel := context.WithCancel(ctx)
+
+	// wire up cancellation from any of the other contexts to our cancel
+	for _, other := range others {
+		if other != nil {
+			stops = append(stops, context.AfterFunc(other, cancel))
+		}
+	}
+
+	// deregister all after funcs on cancel (it's a one-way transition)
+	context.AfterFunc(ctx, stops.Stop)
+
+	_ = cancel // silence go vet for not using cancel on all code paths
+
+	return ctx
 }
 
-// UNEXPORTED
+type stopCallbackSlice []func() bool
 
-// combineContextWorker handles the cancel aggregation logic for CombineContext, batching contexts in groups of up to
-// five, meaning the number of goroutines can be reduced.
-func combineContextWorker(ctx <-chan struct{}, cancel context.CancelFunc, others ...<-chan struct{}) {
-	count := len(others)
-	switch {
-	case count >= 5:
-		if count > 5 {
-			go combineContextWorker(ctx, cancel, others[5:]...)
-		}
-		select {
-		case <-ctx:
-			// already cancelled
-		case <-others[0]:
-			cancel()
-		case <-others[1]:
-			cancel()
-		case <-others[2]:
-			cancel()
-		case <-others[3]:
-			cancel()
-		case <-others[4]:
-			cancel()
-		}
-	case count == 4:
-		select {
-		case <-ctx:
-			// already cancelled
-		case <-others[0]:
-			cancel()
-		case <-others[1]:
-			cancel()
-		case <-others[2]:
-			cancel()
-		case <-others[3]:
-			cancel()
-		}
-	case count == 3:
-		select {
-		case <-ctx:
-			// already cancelled
-		case <-others[0]:
-			cancel()
-		case <-others[1]:
-			cancel()
-		case <-others[2]:
-			cancel()
-		}
-	case count == 2:
-		select {
-		case <-ctx:
-			// already cancelled
-		case <-others[0]:
-			cancel()
-		case <-others[1]:
-			cancel()
-		}
-	case count == 1:
-		select {
-		case <-ctx:
-			// already cancelled
-		case <-others[0]:
-			cancel()
-		}
+func (s stopCallbackSlice) Stop() {
+	for _, stop := range s {
+		stop()
 	}
 }
